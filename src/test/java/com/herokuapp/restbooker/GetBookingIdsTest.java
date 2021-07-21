481cd8a38508de7c11777a5bd9d3fa5b616b3cff
Kamil <qkiz288@gmail.com>
2021-07-21 15:56:51 +0200

    Get /booking/{id} API test

--- src/test/java/com/herokuapp/restbooker/GetBookingIdsTest.java
+++ src/test/java/com/herokuapp/restbooker/GetBookingIdsTest.java
@@ -4,6 +4,7 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.util.List;
 
@@ -13,6 +14,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 
 public class GetBookingIdsTest {
+
+    private static final String ALL_BOOKINGS = "/booking";
+    private static final String BOOKING_BY_ID = "booking/{id}";
 
     RequestSpecification requestSpec;
 
@@ -25,12 +29,27 @@ public class GetBookingIdsTest {
     public void getBookingIdsWithoutFilter() {
         int expectedStatusCode = 200;
 
-        Response response = requestSpec.when().get("/booking");
+        Response response = requestSpec.get(ALL_BOOKINGS);
 
         assertThat(String.format("HTTP Status Code should be %s", expectedStatusCode),
                 response.getStatusCode(), equalTo(expectedStatusCode));
 
         List<Integer> bookingIds = response.jsonPath().getList("bookingId");
         assertThat("bookingId list should not be empty", bookingIds, is(not(empty())));
+    }
+
+    @Test
+    public void getBookingById() {
+        int id = 1;
+        Response response = requestSpec.pathParam("id", id).get(BOOKING_BY_ID);
+
+        String fname = response.jsonPath().get("firstname");
+        String lname = response.jsonPath().get("lastname");
+
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertNotNull(fname, "firstname value shouldn't be null");
+        softAssert.assertNotNull(lname, "lastname value shouldn't be null");
+        softAssert.assertAll();
     }
 }
