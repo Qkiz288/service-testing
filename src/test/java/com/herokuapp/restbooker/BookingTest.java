16bc2a1fb44f999d1afa2e560b6a41a8fba3f83c
Kamil <qkiz288@gmail.com>
2021-07-29 07:52:59 +0200

    Put /booking/{id} API test

--- src/test/java/com/herokuapp/restbooker/BookingTest.java
+++ src/test/java/com/herokuapp/restbooker/BookingTest.java
@@ -6,10 +6,11 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.herokuapp.model.Booking;
 import com.herokuapp.model.BookingDates;
 import com.herokuapp.model.BookingResponse;
+import com.herokuapp.model.Credentials;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -25,23 +26,38 @@ public class BookingTest {
 
     private static final String ALL_BOOKINGS = "/booking";
     private static final String BOOKING_BY_ID = "/booking/{id}";
+    private static final String AUTH = "/auth";
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "password123";
 
-    RequestSpecification requestSpec;
-    ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    private final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    private String token;
 
-    @BeforeMethod
-    public void setUp() {
-        requestSpec = given()
+    private RequestSpecification getRequestSpec() {
+        return given()
                 .contentType(ContentType.JSON)
-//                .accept(ContentType.JSON)
                 .baseUri("https://restful-booker.herokuapp.com");
+    }
+
+    @BeforeClass
+    public void getToken() throws JsonProcessingException {
+        Credentials credentials = Credentials.builder()
+                .username("admin")
+                .password(USERNAME)
+                .password(PASSWORD)
+                .build();
+        String credentialsBody = objectMapper.writeValueAsString(credentials);
+        Response response = getRequestSpec()
+                .body(credentialsBody)
+                .post(AUTH);
+        token = response.jsonPath().get("token");
     }
 
     @Test
     public void getBookingIdsWithoutFilter() {
         int expectedStatusCode = 200;
 
-        Response response = requestSpec.get(ALL_BOOKINGS);
+        Response response = getRequestSpec().get(ALL_BOOKINGS);
 
         assertThat(String.format("HTTP Status Code should be %s", expectedStatusCode),
                 response.getStatusCode(), equalTo(expectedStatusCode));
@@ -53,7 +69,7 @@ public class BookingTest {
     @Test
     public void getBookingById() {
         int id = 1;
-        Response response = requestSpec.pathParam("id", id).get(BOOKING_BY_ID);
+        Response response = getRequestSpec().pathParam("id", id).get(BOOKING_BY_ID);
 
         String fname = response.jsonPath().get("firstname");
         String lname = response.jsonPath().get("lastname");
@@ -67,6 +83,36 @@ public class BookingTest {
 
     @Test
     public void createNewBooking() throws JsonProcessingException {
+        Booking booking = createBasicBooking();
+        String requestBody = objectMapper.writeValueAsString(booking);
+
+        Response response = getRequestSpec()
+                .body(requestBody)
+                .post(ALL_BOOKINGS);
+        assertThat("Response status code should be 200", response.getStatusCode(), equalTo(200));
+
+        BookingResponse bookingResponse = objectMapper.readValue(response.getBody().asString(), BookingResponse.class);
+        assertThat("New booking ID shouldn't be null", bookingResponse.getBookingid(), notNullValue());
+        softAssertAllBookingResponseFields(bookingResponse.getBooking(), booking);
+    }
+
+    @Test
+    public void updateBooking() throws JsonProcessingException {
+        Integer id = 1;
+        Booking booking = createBasicBooking();
+        String requestBody = objectMapper.writeValueAsString(booking);
+
+        Response response = getRequestSpec().body(requestBody)
+                .cookie("token", token)
+                .pathParam("id", id)
+                .put(BOOKING_BY_ID);
+        assertThat("Response status code should be 200", response.getStatusCode(), equalTo(200));
+
+        Booking bookingResponse = objectMapper.readValue(response.getBody().asString(), Booking.class);
+        softAssertAllBookingResponseFields(bookingResponse, booking);
+    }
+
+    private Booking createBasicBooking() {
         String fname = "Kamil";
         String lname = "K";
         Integer totalPrice = 45;
@@ -75,7 +121,7 @@ public class BookingTest {
         LocalDate checkout = LocalDate.now().plusWeeks(1);
         String additionalNeeds = "Nothing";
 
-        Booking booking = Booking.builder()
+        return Booking.builder()
                 .firstname(fname)
                 .lastname(lname)
                 .totalprice(totalPrice)
@@ -86,25 +132,18 @@ public class BookingTest {
                         .build())
                 .additionalneeds(additionalNeeds)
                 .build();
-        String requestBody = objectMapper.writeValueAsString(booking);
+    }
 
-        Response response = requestSpec
-                .body(requestBody)
-                .post(ALL_BOOKINGS);
-        assertThat("Response status code should be 200", response.getStatusCode(), equalTo(200));
-        BookingResponse bookingResponse = objectMapper.readValue(response.getBody().asString(), BookingResponse.class);
-
+    private void softAssertAllBookingResponseFields(Booking booking, Booking expectedBooking) {
         SoftAssert softAssert = new SoftAssert();
-        softAssert.assertNotNull(bookingResponse);
-        softAssert.assertNotNull(bookingResponse.getBookingid());
-        Booking bResponse = bookingResponse.getBooking();
-        softAssert.assertEquals(bResponse.getFirstname(), fname);
-        softAssert.assertEquals(bResponse.getLastname(), lname);
-        softAssert.assertEquals(bResponse.getTotalprice(), totalPrice);
-        softAssert.assertEquals(bResponse.getDepositpaid(), depositPaid);
-        softAssert.assertEquals(bResponse.getBookingdates().getCheckin(), checkin);
-        softAssert.assertEquals(bResponse.getBookingdates().getCheckout(), checkout);
-        softAssert.assertEquals(bResponse.getAdditionalneeds(), additionalNeeds);
+        softAssert.assertNotNull(booking);
+        softAssert.assertEquals(booking.getFirstname(), expectedBooking.getFirstname());
+        softAssert.assertEquals(booking.getLastname(), expectedBooking.getLastname());
+        softAssert.assertEquals(booking.getTotalprice(), expectedBooking.getTotalprice());
+        softAssert.assertEquals(booking.getDepositpaid(), expectedBooking.getDepositpaid());
+        softAssert.assertEquals(booking.getBookingdates().getCheckin(), expectedBooking.getBookingdates().getCheckin());
+        softAssert.assertEquals(booking.getBookingdates().getCheckout(), expectedBooking.getBookingdates().getCheckout());
+        softAssert.assertEquals(booking.getAdditionalneeds(), expectedBooking.getAdditionalneeds());
         softAssert.assertAll();
     }
 
